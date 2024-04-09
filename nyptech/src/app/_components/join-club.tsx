813e5756239d9c3e98f2f2234229adcebdd44ef0
Dennise Catolos <dentolos19@gmail.com>
2024-04-09 08:22:50 +0800

    feat: added basic content for the two sections

--- nyptech/src/app/_components/join-club.tsx
+++ nyptech/src/app/_components/join-club.tsx
@@ -1,9 +1,20 @@
+// Dennise's
+
 const Join = () => {
-    return ( 
-        <div>
-            Join section
-        </div>
-     );
-}
- 
+	return (
+		<div className="text-center">
+			<div className="mb-2 text-3xl font-bold sm:text-4xl md:text-5xl">
+				What are you waiting for?
+			</div>
+			<div className="text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+				Join us now and be part of our community! Exclusively for NYP SIT
+				students!
+			</div>
+			<button className="mt-4 btn btn-primary" type="button">
+				Join us now!
+			</button>
+		</div>
+	);
+};
+
 export default Join;
--- nyptech/src/app/_components/sign-up.tsx
+++ nyptech/src/app/_components/sign-up.tsx
@@ -1,9 +1,18 @@
+// Dennise's
+
 const SignUp = () => {
-    return ( 
-        <div>
-            SignUp section
-        </div>
-     );
-}
- 
+	return (
+		<div className="text-center">
+			<div className="mb-2 text-3xl font-bold sm:text-4xl md:text-5xl">Sign Up</div>
+			<div className="text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+				Sign up for our newsletter and get the latest updates on our events and
+				activities!
+			</div>
+			<button className="mt-4 btn btn-primary" type="button">
+				Sign up now!
+			</button>
+		</div>
+	);
+};
+
 export default SignUp;

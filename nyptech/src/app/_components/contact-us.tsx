bc78dbb3185c8ab2f4d4cef04f94b871f5173a32
Sven <iamsven2005@gmail.com>
2024-04-11 13:44:41 +0800

    Added a video

--- nyptech/src/app/_components/contact-us.tsx
+++ nyptech/src/app/_components/contact-us.tsx
@@ -1,6 +1,6 @@
 const Contact = () => {
     return (
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeJACQhidKjvHp6_6adFbNzZNlxqjpAEwIIWSMg_nTv6MqFWA/viewform?embedded=true" width="100%" height="600" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeJACQhidKjvHp6_6adFbNzZNlxqjpAEwIIWSMg_nTv6MqFWA/viewform?embedded=true" width="100%" height="700">Loading…</iframe>
     );
 }
 

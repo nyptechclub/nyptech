6950700d5d32f822d2dbdabfaf31ef847232ed09
Dennise Catolos <dentolos19@gmail.com>
2024-04-12 18:15:57 +0800

    style: update sign up section

--- nyptech/src/app/_components/sign-up.tsx
+++ nyptech/src/app/_components/sign-up.tsx
@@ -1,24 +1,33 @@
-import socials from './socials.json'; // Assuming socials.json contains the social media links
-import React from 'react';
+import Link from "next/link";
+import socials from "./socials.json"; // Assuming socials.json contains the social media links
 
 const SignUp = () => {
 	return (
-		<section className="w-full py-8 text-center">
-			<div className="mb-2 text-3xl font-bold sm:text-4xl md:text-5xl">Get to know us better!</div>
+		<section className="py-8 w-fit flex flex-col items-center text-center">
+			<div className="mb-2 text-3xl font-bold sm:text-4xl md:text-5xl">
+				Get to know us better!
+			</div>
 			<div className="text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
 				Sign up for our newsletter and get the latest updates on our events and
 				activities!
 			</div>
-			<div className="items-center text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed join">
-      {socials.map((social, index) => (
-        <a key={index} className="avatar w-10 h-10 rounded-full" href={social.link}>
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-10 rounded-full">
-		  <path d={social.icon} />
-		</svg>
-	  </a>
-	      ))}
-    </div><br>
-	</br>
+			<div className="mt-2 flex gap-1">
+				{socials.map((social, index) => (
+					<Link
+						key={index}
+            className={"transition hover:scale-110"}
+						href={social.link}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 32 32"
+							className="w-10 rounded-full"
+						>
+							<path d={social.icon} />
+						</svg>
+					</Link>
+				))}
+			</div>
 			<button className="mt-4 btn btn-glow" type="button">
 				Sign up now!
 			</button>

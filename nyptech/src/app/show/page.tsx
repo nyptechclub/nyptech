bb8f901a0a391a1e7faafd323e1cbbbdcdd4224f
iamsven2005 <iamsven2005@gmail.com>
2024-04-29 22:13:35 +0800

    Added tempory showcase page

---
+++ nyptech/src/app/show/page.tsx
@@ -0,0 +1,10 @@
+const show = () => {
+    return ( 
+        <div className="text-3xl">
+            <div className="hero">Stay Tuned For Upcoming News!</div>
+            
+        </div>
+     );
+}
+ 
+export default show;

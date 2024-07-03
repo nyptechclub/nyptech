bfe45a5a0bf9a07d55372dac13c21dc155ffd46d
Dennise Catolos <dentolos19@gmail.com>
2024-07-03 18:35:15 +0800

    Added projects in docs

---
+++ src/app/docs/projects/page.tsx
@@ -0,0 +1,24 @@
+import projects from "@/data/projects.json";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <main className={"p-4 h-full flex flex-col gap-2"}>
+      {projects.map((project) => (
+        <Link
+          key={project.name}
+          className={"card sm:card-side bg-base-100 shadow-lg transition hover:bg-base-200"}
+          href={project.link}
+        >
+          <figure>
+            <img className={"sm:size-[120px]"} src={project.imageSrc} alt={project.name} />
+          </figure>
+          <div className={"card-body"}>
+            <h1 className={"card-title"}>{project.name}</h1>
+            <p>{project.description}</p>
+          </div>
+        </Link>
+      ))}
+    </main>
+  );
+}

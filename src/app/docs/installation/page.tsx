d6ba36617149ee1e02bef6947a5375cfe8373496
Tan Sven <iamsven2005@gmail.com>
2024-08-02 17:03:38 +0800

    Added flask starter kit

--- src/app/docs/installation/page.tsx
+++ src/app/docs/installation/page.tsx
@@ -20,7 +20,15 @@ export default function Page() {
               "https://vercel.com/new/clone?repository-url=https://github.com/nyp-tech/nyp-tech/tree/main/flask-npx&demo-title=Flask%20Hello%20World&demo-description=Use%20Python%20on%20Vercel%20with%20Serverless%20Functions%20using%20the%20Python%20Runtime.&demo-url=https://python-hello-world.vercel.app/&demo-image=https://assets.vercel.com/image/upload/v1669994600/random/python.png"
             }
           >
-            Flask
+            Flask Empty
+          </Link>
+          <Link
+            className={"btn"}
+            href={
+              "https://vercel.com/new/clone?repository-url=https://github.com/nyp-tech/nyp-tech/tree/main/mendel&demo-title=Flask%mendel&demo-description=Use%20Python%20on%20Vercel%20with%20Serverless%20Functions%20using%20the%20Python%20Runtime.&demo-url=https://python-hello-world.vercel.app/&demo-image=https://assets.vercel.com/image/upload/v1669994600/random/python.png"
+            }
+          >
+            Flask Starter
           </Link>
           <Link
             className={"btn"}

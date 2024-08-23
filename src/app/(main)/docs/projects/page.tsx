116ca2edd4368788f52aed8d814598ea98c60bb1
Tan Sven <iamsven2005@gmail.com>
2024-08-23 23:31:01 +0800

    added a visit button instead of the entire card for accesbility

--- src/app/(main)/docs/projects/page.tsx
+++ src/app/(main)/docs/projects/page.tsx
@@ -5,20 +5,26 @@ export default function Page() {
   return (
     <main className={"p-4 flex flex-col gap-2"}>
       {projects.map((project) => (
-        <Link
+        <div
           key={project.name}
           className={"card sm:card-side bg-base-100 shadow-lg transition hover:bg-base-200"}
-          href={project.link}
         >
           <figure>
             <img className={"sm:size-[120px]"} src={project.imageSrc} alt={project.name} />
           </figure>
           <div className={"card-body"}>
-            <h1 className={"card-title"}>{project.name}</h1>
-          <Link href={`https://nyptech-learn.vercel.app/room/${project.name}`}></Link>
+            <div className={"card-title"}           
+            >{project.name}</div>
+            <div className="flex gap-2">
+            <Link href={project.link} className="btn">
+          Visit
+          </Link>
+          <Link href={`https://nyptech-learn.vercel.app/room/${project.name}`} className="btn">Feedback</Link>
+            </div>
+          
             <p>{project.description}</p>
           </div>
-        </Link>
+        </div>
       ))}
     </main>
   );

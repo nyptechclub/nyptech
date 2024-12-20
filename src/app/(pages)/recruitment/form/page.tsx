1b89935eb6ed3e284f29f72df46a4957861745ed
Dennise Catolos <dentolos19@gmail.com>
2024-12-20 13:06:06 +0800

    Added recruitment form

---
+++ src/app/(pages)/recruitment/form/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { FilloutStandardEmbed } from "@fillout/react";
+
+export default function Page() {
+  return (
+    <main>
+      <FilloutStandardEmbed filloutId={"4o5NLfDpuPus"} />
+    </main>
+  );
+}

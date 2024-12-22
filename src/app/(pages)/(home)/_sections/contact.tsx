6ba1c352285962af5787544e7505c98103705960
Dennise Catolos <dentolos19@gmail.com>
2024-12-22 09:32:20 +0800

    Updated responsiveness of landing page

--- src/app/(pages)/(home)/_sections/contact.tsx
+++ src/app/(pages)/(home)/_sections/contact.tsx
@@ -1,42 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const iconSize = 40;
 const socials = [
   {
     name: "Discord",
     url: "https://go.nyptech.club/discord",
-    icon: (
-      <Image className={"invert"} src={"/assets/icons/discord.svg"} alt={"Icon"} width={iconSize} height={iconSize} />
-    ),
+    icon: <Image className={"invert"} src={"/assets/icons/discord.svg"} alt={"Discord"} fill />,
   },
   {
     name: "WhatsApp",
     url: "https://go.nyptech.club/whatsapp",
-    icon: (
-      <Image className={"invert"} src={"/assets/icons/whatsapp.svg"} alt={"Icon"} width={iconSize} height={iconSize} />
-    ),
+    icon: <Image className={"invert"} src={"/assets/icons/whatsapp.svg"} alt={"WhatsApp"} fill />,
   },
   {
     name: "Telegram",
     url: "https://go.nyptech.club/telegram",
-    icon: (
-      <Image className={"invert"} src={"/assets/icons/telegram.svg"} alt={"Icon"} width={iconSize} height={iconSize} />
-    ),
+    icon: <Image className={"invert"} src={"/assets/icons/telegram.svg"} alt={"Telegram"} fill />,
   },
   {
     name: "Instagram",
     url: "https://go.nyptech.club/instagram",
-    icon: (
-      <Image className={"invert"} src={"/assets/icons/instagram.svg"} alt={"Icon"} width={iconSize} height={iconSize} />
-    ),
+    icon: <Image className={"invert"} src={"/assets/icons/instagram.svg"} alt={"Instagram"} fill />,
   },
   {
     name: "LinkedIn",
     url: "https://go.nyptech.club/linkedin",
-    icon: (
-      <Image className={"invert"} src={"/assets/icons/linkedin.svg"} alt={"Icon"} width={iconSize} height={iconSize} />
-    ),
+    icon: <Image className={"invert"} src={"/assets/icons/linkedin.svg"} alt={"LinkedIn"} fill />,
   },
 ];
 
@@ -45,13 +34,15 @@ export default function ContactSection() {
     <section className={"hero min-h-full lg:min-h-[80%]"}>
       <div className={"hero-content"}>
         <div className={"text-center"}>
-          <div className={"mb-4 text-6xl font-bold"}>Get to know us better!</div>
-          <div className={"mb-8 text-lg"}>Join one of our social media channels to get latest updates and opportunities from us!</div>
-          <div className={"flex justify-center gap-8"}>
+          <div className={"mb-4 text-6xl font-bold max-md:text-4xl"}>Get to know us better!</div>
+          <div className={"max-md:text-md mb-8 text-lg"}>
+            Join one of our social media channels to get latest updates and opportunities from us!
+          </div>
+          <div className={"flex justify-center gap-8 max-md:gap-6"}>
             {socials.map((social) => (
               <Link
                 key={social.name}
-                className={"grid aspect-square place-items-center transition hover:scale-95"}
+                className={"relative aspect-square w-12 transition hover:scale-110 max-md:w-8"}
                 href={social.url}
               >
                 {social.icon}
--- src/app/(pages)/(home)/_sections/feedback.tsx
+++ src/app/(pages)/(home)/_sections/feedback.tsx
@@ -5,8 +5,8 @@ export default function FeedbackSection() {
     <section className={"hero min-h-full lg:min-h-[80%]"}>
       <div className={"hero-content"}>
         <div className={"text-center"}>
-          <div className={"mb-4 text-6xl font-bold"}>Do you have anything for us?</div>
-          <div className={"mb-8 text-lg"}>Don&apos;t just be an bystander and get involved!</div>
+          <div className={"mb-4 text-6xl font-bold max-md:text-4xl"}>Do you have anything for us?</div>
+          <div className={"max-md:text-md mb-8 text-lg"}>Don&apos;t just be an bystander and get involved!</div>
           <Link className={"btn btn-primary"} href={"/contact"}>
             Send us a message!
           </Link>

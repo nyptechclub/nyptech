2696e549630761f295efeb610d0ce62420fa70d2
Dennise Catolos <dentolos19@gmail.com>
2024-12-22 17:36:11 +0800

    Added custom backgrounds

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,11 +1,15 @@
 import CommitteeSection from "@/app/(pages)/about/_sections/committee";
 import SubcommitteeSection from "@/app/(pages)/about/_sections/subcommittee";
+import AnimatedGridPattern from "@/components/ui/animated-grid-pattern";
 
 export default function Page() {
   return (
-    <main>
-      <CommitteeSection />
-      <SubcommitteeSection />
-    </main>
+    <>
+      <AnimatedGridPattern className={"inset-x-0 inset-y-[-30%] h-[200%] skew-y-12"} />
+      <main>
+        <CommitteeSection />
+        <SubcommitteeSection />
+      </main>
+    </>
   );
 }
--- src/app/(pages)/learn/page.tsx
+++ src/app/(pages)/learn/page.tsx
@@ -1,26 +1,28 @@
+import DotPattern from "@/components/ui/dot-pattern";
 import Link from "next/link";
 
 export default function Page() {
   return (
-    <main>
-      <section className={"hero h-full"}>
-        <div className={"hero-content"}>
-          <div className={"max-w-lg text-center"}>
-            <img
-              className={"mx-auto mb-8 size-[100px] lg:size-[150px]"}
-              src={"https://learn.nyptech.club/wizard.png"}
-              alt={"Logo"}
-            />
-            <div className={"mb-4 text-4xl font-bold"}>Live. Laugh. Learn.</div>
-            <div>
-              Learn something new every day. It&apos;s good for your brain.
+    <>
+      <DotPattern className={"[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]"} />
+      <main>
+        <section className={"hero h-full"}>
+          <div className={"hero-content"}>
+            <div className={"max-w-lg text-center"}>
+              <img
+                className={"mx-auto mb-8 size-[100px] lg:size-[150px]"}
+                src={"https://learn.nyptech.club/wizard.png"}
+                alt={"Logo"}
+              />
+              <div className={"mb-4 text-4xl font-bold"}>Live. Laugh. Learn.</div>
+              <div>Learn something new every day. It&apos;s good for your brain.</div>
+              <Link className={"btn btn-primary mt-6"} href={"https://learn.nyptech.club"}>
+                Start learning now!
+              </Link>
             </div>
-            <Link className={"btn btn-primary mt-6"} href={"https://learn.nyptech.club"}>
-              Start learning now!
-            </Link>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </>
   );
 }

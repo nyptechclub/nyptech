38f8b3b650bf429a0b3b7eadcea93a8aca9f4678
Dennise Catolos <dentolos19@gmail.com>
2025-02-27 23:52:00 +0800

    Updated innovation section

--- src/app/(pages)/(home)/_sections/innovation.tsx
+++ src/app/(pages)/(home)/_sections/innovation.tsx
@@ -1,89 +1,47 @@
-"use client";
-
-import { cn } from "@/lib/utils";
-import { useEffect, useRef, useState } from "react";
-
-export default function InnovationSection({ className }: { className?: string }) {
-  const [isVisible, setIsVisible] = useState(false);
-  const sectionRef = useRef<HTMLElement>(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setIsVisible(true);
-          observer.unobserve(entry.target);
-        }
-      },
-      {
-        root: null,
-        rootMargin: "0px",
-        threshold: 0.1,
-      },
-    );
-
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current);
-    }
-
-    return () => {
-      if (sectionRef.current) {
-        observer.unobserve(sectionRef.current);
-      }
-    };
-  }, []);
-
+export default function InnovationSection(props: { className?: string }) {
   return (
-    <section
-      ref={sectionRef}
-      className={cn("hero opacity-0 transition-opacity duration-1000 overflow-x-auto", { "opacity-100": isVisible }, className)}
-    >
-      <div className="gap-8 max-lg:flex-col">
-        <div className="items-center space-y-4">
-          <div className={"mb-10 text-6xl text-center font-bold"}>Faster Iteration, More Innovation</div>
-          <div className="flex items-center gap-10 sm:flex-col md:flex-row">
-            <div className="card items-center">
-              <div className="avatar">
-                <div className="mask mask-squircle w-80">
-                  <img src="/assets/blog/workshop.jpeg" />
-                </div>
-              </div>
-              <div className="card-body">
-                <h2 className="card-title">Workshops</h2>
-                <p>
-                  We have a list of upcoming workshops catered just for you to learn more about what it takes to start a
-                  SAAS
-                </p>
-              </div>
-            </div>
-            <div className="card items-center">
-              <div className="avatar">
-                <div className="mask mask-triangle w-80">
-                  <img src="/assets/blog/hackathon.jpeg" />
-                </div>
-              </div>
-              <div className="card-body">
-                <h2 className="card-title">Join The Community</h2>
-                <p>
-                  Join a group of like minded people to brainstorm, develop and maintain code, get a chance to work with
-                  real-world clients.
-                </p>
-              </div>
-            </div>
-            <div className="card items-center">
-              <div className="avatar">
-                <div className="mask w-80 rounded-full">
-                  <img src="/assets/blog/group.jpg" />
-                </div>
-              </div>
-              <div className="card-body">
-                <h2 className="card-title">Access capital</h2>
-                <p>
-                  We aim to provide you with grands and funding opportunities as well as advise on how to better pursue
-                  them.
-                </p>
-              </div>
-            </div>
+    <section className={props.className}>
+      <div className={"mb-16 text-center"}>
+        <h2 className={"text-4xl font-bold"}>Faster Iteration, More Innovation</h2>
+        <p className={"mt-4 text-lg"}>
+          Empowering breakthrough ideas by streamlining iterations and driving continuous innovation.
+        </p>
+      </div>
+      <div className={"container mx-auto grid grid-cols-3 gap-8"}>
+        <div className={"card bg-base-300"}>
+          <figure>
+            <img className={"h-60 object-cover"} src={"/assets/blog/workshop.jpeg"} about={"Workshop"} />
+          </figure>
+          <div className={"card-body"}>
+            <h2 className={"card-title text-xl"}>Learn From Workshops</h2>
+            <p className={"text-current/80"}>
+              We have a list of upcoming workshops catered just for you to learn more about what it takes to start a
+              SaaS.
+            </p>
+          </div>
+        </div>
+        <div className={"card bg-base-300"}>
+          <figure>
+            <img className={"h-60 object-cover"} src={"/assets/blog/hackathon.jpeg"} about={"Hackathon"} />
+          </figure>
+          <div className={"card-body"}>
+            <h2 className={"card-title text-xl"}>Join The Community</h2>
+            <p className={"text-current/80"}>
+              Join a group of like minded people to brainstorm, develop and maintain code, get a chance to work with
+              real-world clients.
+            </p>
+          </div>
+        </div>
+        <div className={"card bg-base-300"}>
+          <figure>
+            <img className={"h-60 object-cover"} src={"/assets/blog/group.jpg"} about={"Group"} />
+          </figure>
+          <div className={"card-body"}>
+            <h2 className={"card-title text-xl"}>Access Capital</h2>
+            <p className={"text-current/80"}>
+              We aim to provide you with grands and funding opportunities as well as advise on how to better pursue
+              them.
+            </p>
           </div>
         </div>
       </div>
